ac8484fe5bce109dd1a851da2b964673f8b350be
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 15:54:33 +0200

    changed the isOpen

--- components/UploadModal.tsx
+++ components/UploadModal.tsx
@@ -9,14 +9,18 @@ import Modal from './Modal';
 import Button from './Button';
 
 const UploadModal = () => {
-
-
+    const uploadModal = useUploadModal();
+    const onChange = (open: boolean) => {
+        if (!open) {
+          uploadModal.onClose();
+        }
+      }
 
   return (
     <Modal
       title="Add a song"
       description="Upload an mp3 file"
-      isOpen
+      isOpen={uploadModal.isOpen}
       onChange={() => {}}
     >
      
