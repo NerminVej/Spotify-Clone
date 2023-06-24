74109e7a6a6cbfea0b7ba075edb41edba10fc167
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 09:31:28 +0200

    added some routes

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,10 +1,29 @@
-import React, { Children } from 'react'
+import React, { Children, useMemo } from 'react'
+import { usePathname } from "next/navigation";
+
+
 interface SidebarProps {
     children: React.ReactNode;
 }
 const Sidebar: React.FC<SidebarProps> = ({
     children
 }) => {
+
+const pathname = usePathname();
+
+const routes = useMemo(() => [
+    {
+        label: "Home",
+        active: pathname !== "/search",
+        href: "/",
+    },
+    {
+        label: "Search",
+        active: pathname === "/search",
+        href: "search",
+    }
+], [pathname]);
+
   return (
     <div>
         {children}
