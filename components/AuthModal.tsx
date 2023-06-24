c9bd5df93a9077762a0c562badeea30d0a65ec6d
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 14:17:20 +0200

    added Modal to the AuthModal component

---
+++ components/AuthModal.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+import React from "react";
+import Modal from "./Modal";
+
+("use client");
+
+const AuthModal = () => {
+  return <Modal title="Welcome Back"></Modal>;
+};
+
+export default AuthModal;
