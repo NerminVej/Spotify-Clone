1675e52536130f178c63db3d1bd546d8a1fdf336
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 11:10:14 +0200

    added the image element

--- components/ListItem.tsx
+++ components/ListItem.tsx
@@ -1,9 +1,49 @@
-import React from 'react'
+"use client";
 
-function ListItem() {
-  return (
-    <div>ListItem</div>
-  )
+import { useRouter } from "next/navigation";
+import React from "react";
+import Image from "next/image";
+
+interface ListItemProps {
+  image: string;
+  name: string;
+  href: string;
 }
 
-export default ListItem
+const ListItem: React.FC<ListItemProps> = ({ image, name, href }) => {
+  const router = useRouter();
+
+  const onClick = () => {
+    // add authentication before push
+    router.push(href);
+  };
+
+  return (
+    <button
+      className="
+        relative
+        group
+        flex
+        items-center
+        roounded-md
+        overflow-hidden
+        gap-x-4
+        bg-neutral-100/10hover:bg-neutral-100/20
+        transition
+        pr-4
+    "
+    >
+      <div
+        className="
+        relative
+        min-h-[64px]
+        min-w-[64px]
+      "
+      >
+        <Image className="object-cover" fill src={image} alt="Image" />
+      </div>
+    </button>
+  );
+};
+
+export default ListItem;
