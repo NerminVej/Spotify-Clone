55ab334edce28a97465191fc5d77717965d4ecdd
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 13:50:18 +0200

    added all the types for TypeScript to work with Stripe

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
               font-smibold"
           ></h1>
           <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
-            <ListItem 
+            <ListItem
               image="/images/liked.png"
               name="Liked songs"
               href="liked"
@@ -31,12 +31,10 @@ export default function Home() {
         </div>
       </Header>
       <div className="mt-2 mb-7 px-6">
-<div className="flex justify-between items-center">
-<h1 className="text-white text-2xl font-semibold">Newest songs</h1>
-</div>
-<div>
-  List of Songs!
-</div>
+        <div className="flex justify-between items-center">
+          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+        </div>
+        <div>List of Songs!</div>
       </div>
     </div>
   );
