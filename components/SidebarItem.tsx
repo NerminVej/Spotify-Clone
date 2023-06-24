8137f75af308654c81a4b344e433dbcb5f46d303
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 09:46:53 +0200

    added a nice hovering effect for the SidebarItem

--- components/SidebarItem.tsx
+++ components/SidebarItem.tsx
@@ -1,8 +1,40 @@
+import Link from 'next/link';
 import React from 'react'
+import { IconType } from 'react-icons'
+import { twMerge } from "tailwind-merge";
 
-const SidebarItem = () => {
+interface SidebarItemProps {
+    icon: IconType;
+    label: string;
+    active?: boolean;
+    href: string;
+}
+
+const SidebarItem: React.FC<SidebarItemProps> = ({
+    icon,
+    label,
+    active,
+    href
+}) => {
   return (
-    <div>SidebarItem</div>
+    <Link href={href}
+    className={twMerge(`
+    flex
+    flex-row
+    h-auto
+    items-center
+    w-full
+    gap-x-4
+    text-md
+    font-medium
+    cursor-pointer
+    hover:text-white
+    transition
+    text-neutral-400
+    py-1
+    `)}>
+        Sidebar Item
+    </Link>
   )
 }
 
