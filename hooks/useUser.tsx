7c567939e8615cd90cd95458d593fb1d5295e875
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 13:57:17 +0200

    implemented the useUser hook

--- hooks/useUser.tsx
+++ hooks/useUser.tsx
@@ -66,4 +66,21 @@ export const MyUserContextProvider = (props: Props) => {
     }
   }, [user, isLoadingUser]);
 
- 
+  const value = {
+    accessToken,
+    user,
+    userDetails,
+    isLoading: isLoadingUser || isLoadingData,
+    subscription
+  };
+
+  return <UserContext.Provider value={value} {...props} />;
+};
+
+export const useUser = () => {
+  const context = useContext(UserContext);
+  if (context === undefined) {
+    throw new Error(`useUser must be used within a MyUserContextProvider.`);
+  }
+  return context;
+};
