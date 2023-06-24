7436c64e859218be976e6602e17749cd44b03887
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 10:44:01 +0200

    added the Button component

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,6 +5,8 @@ import React from "react";
 import { twMerge } from "tailwind-merge";
 import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
+import { BiSearch } from "react-icons/bi";
+import Button from "./Button";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -102,8 +104,24 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           transition
           "
           >
-            <HiHome className="text-black" size={20} />
+            <BiSearch className="text-black" size={20} />
           </button>
+        </div>
+
+        <div
+          className="
+            flex
+            justify-between
+            items-center
+            gap-x-4
+
+            "
+        >
+          <>
+          <div>
+            <Button />
+          </div>
+          </>
         </div>
       </div>
     </div>
