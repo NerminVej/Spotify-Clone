f87658ba72c7c00d6834ecbe0a33b25fa271315c
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 09:55:04 +0200

    added some styling to the Library

--- components/Library.tsx
+++ components/Library.tsx
@@ -1,11 +1,34 @@
 "use client";
 
-import React from 'react'
+import React from "react";
+import { TbPlaylist } from "react-icons/tb";
 
 const Library = () => {
+  const onClick = () => {
+    // Handle upload
+  };
   return (
-    <div>Library</div>
-  )
-}
+    <div className="flex flex-col">
+      <div
+        className="
+            flex
+            items-center
+            justify-between
+            px-5
+            pt-4
+            "
+      >
+        <div
+          className="
+            inline-flex
+            items-center
+            gap-x-2
+            "
+        ></div>
+        <TbPlaylist />
+      </div>
+    </div>
+  );
+};
 
-export default Library
+export default Library;
