3ccee0f109db156ede41d54eef51b7f5862eb999
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 09:27:09 +0200

    added the Sidebar component

---
+++ components/Sidebar.tsx
@@ -0,0 +1,15 @@
+import React, { Children } from 'react'
+interface SidebarProps {
+    children: React.ReactNode;
+}
+const Sidebar: React.FC<SidebarProps> = ({
+    children
+}) => {
+  return (
+    <div>
+        {children}
+    </div>
+  )
+}
+
+export default Sidebar
