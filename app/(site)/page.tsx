ffb59aa000c6c3f6c14ede6a8b03123e412f95c4
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 11:03:32 +0200

    added the ListItem component

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,4 +1,5 @@
 import Header from "@/components/Header";
+import ListItem from "@/components/ListItem";
 
 export default function Home() {
   return (
@@ -12,7 +13,19 @@ export default function Home() {
     overflow-y-auto
     "
     >
-      <Header>Header</Header>
+      <Header>
+        <div className="mb-2">
+          <h1
+            className="
+            text-white
+              text-3xl
+              font-smibold"
+          ></h1>
+          <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
+            <ListItem />
+          </div>
+        </div>
+      </Header>
     </div>
   );
 }
