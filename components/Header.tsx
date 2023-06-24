42bcb2bfb5a44318572ac7750ccf9a3b9e97fcfb
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 14:26:42 +0200

    added the AuthModal functionality to the frontend on the signup and login button click

--- components/Header.tsx
+++ components/Header.tsx
@@ -7,6 +7,7 @@ import { RxCaretLeft, RxCaretRight } from "react-icons/rx";
 import { HiHome } from "react-icons/hi";
 import { BiSearch } from "react-icons/bi";
 import Button from "./Button";
+import useAuthModal from "@/hooks/useAuthModal";
 
 interface HeaderProps {
   children: React.ReactNode;
@@ -14,6 +15,8 @@ interface HeaderProps {
 }
 
 const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  
+  const authModal = useAuthModal();
   const router = useRouter();
 
   const handleLogout = () => {
@@ -62,7 +65,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             <RxCaretLeft className="text-white" size={35} />
           </button>
           <button
-            onClick={() => router.forward()}
+            onClick={() => {}}
             className="
             rounded-full
             bg-black
@@ -112,7 +115,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
           <>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="
               bg-transparent
               text-neutral-300
@@ -124,7 +127,7 @@ const Header: React.FC<HeaderProps> = ({ children, className }) => {
             </div>
             <div>
               <Button
-                onClick={() => {}}
+                onClick={authModal.onOpen}
                 className="
                 bg-white
                 px-6
