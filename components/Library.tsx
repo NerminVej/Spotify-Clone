5739a4861f59e2b37b82645119bfb976d232354b
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 09:59:10 +0200

    added some more styling and a hover effect to the plus sign

--- components/Library.tsx
+++ components/Library.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { TbPlaylist } from "react-icons/tb";
+import { AiOutlinePlus } from "react-icons/ai";
 
 const Library = () => {
   const onClick = () => {
@@ -24,8 +25,39 @@ const Library = () => {
             items-center
             gap-x-2
             "
-        ></div>
-        <TbPlaylist />
+        >
+          <TbPlaylist className="text-neutral-400" size={26} />
+          <p
+            className="
+            text-neutral-400
+            font-medium
+            text-md
+            "
+          >
+            Your Library
+          </p>
+        </div>
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
+          className="
+            text-neutral-400
+            cursor-pointer
+            hover:text-white
+            transition
+            "
+        />
+      </div>
+      <div
+        className="
+        flex
+        flex-col
+        gap-y-2
+        mt-4
+        px-3
+        "
+      >
+        List of Songs!
       </div>
     </div>
   );
