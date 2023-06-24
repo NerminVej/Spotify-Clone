b34458a577833d43ced4441b0580415f29649f18
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-24 10:35:40 +0200

    added a green gradient

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,29 +1,51 @@
 "use client";
 
-import { useRouter } from 'next/navigation';
-import React from 'react'
+import { useRouter } from "next/navigation";
+import React from "react";
+import { twMerge } from "tailwind-merge";
 
 interface HeaderProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
-const Header: React.FC<HeaderProps> = ({
-    children,
-    className
-}) => {
-    const router = useRouter();
+const Header: React.FC<HeaderProps> = ({ children, className }) => {
+  const router = useRouter();
 
-    const handleLogout = () => {
-        // Handle logout later
-    }
+  const handleLogout = () => {
+    // Handle logout later
+  };
 
   return (
-    
-    <div>
-
+    <div
+      className={twMerge(
+        `
+    h-fit
+    bg-gradient-to-b
+    from-emerald-800
+    p-6
+    `,
+        className
+      )}
+    >
+      <div
+        className="w-full
+        mb-4
+        flex-items-center
+        justify-between
+        "
+      >
+        <div
+          className="
+            hidden
+            md:flex
+            gap-x-2
+            items-center
+            "
+        ></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
