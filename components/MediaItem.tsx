e5f345c4fd032ec59bcf3c87b5269b66d25cfcbc
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-25 08:32:22 +0200

    added the getSongsByTitle action

--- components/MediaItem.tsx
+++ components/MediaItem.tsx
@@ -11,10 +11,7 @@ interface MediaItemProps {
   onClick?: (id: string) => void;
 }
 
-const MediaItem: React.FC<MediaItemProps> = ({
-  data,
-  onClick,
-}) => {
+const MediaItem: React.FC<MediaItemProps> = ({ data, onClick }) => {
   const player = usePlayer();
   const imageUrl = useLoadImage(data);
 
@@ -22,11 +19,11 @@ const MediaItem: React.FC<MediaItemProps> = ({
     if (onClick) {
       return onClick(data.id);
     }
-  
+
     return player.setId(data.id);
   };
 
-  return ( 
+  return (
     <div
       onClick={handleClick}
       className="
@@ -40,7 +37,7 @@ const MediaItem: React.FC<MediaItemProps> = ({
         rounded-md
       "
     >
-      <div 
+      <div
         className="
           relative 
           rounded-md 
@@ -51,19 +48,17 @@ const MediaItem: React.FC<MediaItemProps> = ({
       >
         <Image
           fill
-          src={imageUrl || "/images/music-placeholder.png"}
+          src={imageUrl || "/images/liked.png"}
           alt="MediaItem"
           className="object-cover"
         />
       </div>
       <div className="flex flex-col gap-y-1 overflow-hidden">
         <p className="text-white truncate">{data.title}</p>
-        <p className="text-neutral-400 text-sm truncate">
-          By {data.author}
-        </p>
+        <p className="text-neutral-400 text-sm truncate">By {data.author}</p>
       </div>
     </div>
   );
-}
- 
+};
+
 export default MediaItem;
