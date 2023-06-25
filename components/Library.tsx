8d05ab700ea7089a0a3b8e817904c741899528fb
Nermin Vejzovic <104377893+NerminVej@users.noreply.github.com>
2023-06-25 09:07:41 +0200

    fixed some formatting

--- components/Library.tsx
+++ components/Library.tsx
@@ -7,7 +7,6 @@ import { Song } from "@/types";
 import useUploadModal from "@/hooks/useUploadModal";
 import { useUser } from "@/hooks/useUser";
 import useAuthModal from "@/hooks/useAuthModal";
-import useSubscribeModal from "@/hooks/useSubscribeModal";
 import useOnPlay from "@/hooks/useOnPlay";
 
 import MediaItem from "./MediaItem";
@@ -16,13 +15,10 @@ interface LibraryProps {
   songs: Song[];
 }
 
-const Library: React.FC<LibraryProps> = ({
-  songs
-}) => {
+const Library: React.FC<LibraryProps> = ({ songs }) => {
   const { user, subscription } = useUser();
   const uploadModal = useUploadModal();
   const authModal = useAuthModal();
-  const subscribeModal = useSubscribeModal();
 
   const onPlay = useOnPlay(songs);
 
@@ -31,25 +27,19 @@ const Library: React.FC<LibraryProps> = ({
       return authModal.onOpen();
     }
 
-    if (!subscription) {
-      return subscribeModal.onOpen();
-    }
-
     return uploadModal.onOpen();
-  }
+  };
 
-  return ( 
+  return (
     <div className="flex flex-col">
       <div className="flex items-center justify-between px-5 pt-4">
         <div className="inline-flex items-center gap-x-2">
           <TbPlaylist className="text-neutral-400" size={26} />
-          <p className="text-neutral-400 font-medium text-md">
-            Your Library
-          </p>
+          <p className="text-neutral-400 font-medium text-md">Your Library</p>
         </div>
-        <AiOutlinePlus 
-          onClick={onClick} 
-          size={20} 
+        <AiOutlinePlus
+          onClick={onClick}
+          size={20}
           className="
             text-neutral-400 
             cursor-pointer 
@@ -60,15 +50,15 @@ const Library: React.FC<LibraryProps> = ({
       </div>
       <div className="flex flex-col gap-y-2 mt-4 px-3">
         {songs.map((item) => (
-          <MediaItem 
-            onClick={(id: string) => onPlay(id)} 
-            key={item.id} 
+          <MediaItem
+            onClick={(id: string) => onPlay(id)}
+            key={item.id}
             data={item}
           />
         ))}
       </div>
     </div>
-   );
-}
- 
+  );
+};
+
 export default Library;
--- hooks/useOnPlay.ts
+++ hooks/useOnPlay.ts
@@ -1,13 +1,11 @@
 import { Song } from "@/types";
 
 import usePlayer from "./usePlayer";
-import useSubscribeModal from "./useSubscribeModal";
 import useAuthModal from "./useAuthModal";
 import { useUser } from "./useUser";
 
 const useOnPlay = (songs: Song[]) => {
   const player = usePlayer();
-  const subscribeModal = useSubscribeModal();
   const authModal = useAuthModal();
   const { subscription, user } = useUser();
 
@@ -16,9 +14,7 @@ const useOnPlay = (songs: Song[]) => {
       return authModal.onOpen();
     }
 
-    if (!subscription) {
-      return subscribeModal.onOpen();
-    }
+
 
     player.setId(id);
     player.setIds(songs.map((song) => song.id));
